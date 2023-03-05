2fe3841139016422fa22982874ed495dabda1992
Matvey <klanffyt@gmail.com>
2023-03-05 13:26:08 +0200

    Initial Commit

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrows/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrows/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
